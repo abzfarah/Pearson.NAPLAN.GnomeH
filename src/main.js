60deb8cc73d972bb03945832770490b22d518167
Abdi Farah <abdi.farah@pearson.com>
2016-09-27 16:16:38 +1000

    Resolve conflict.

--- src/main.js
+++ src/main.js
@@ -1,47 +1,32 @@
-import React from 'react'
-import ReactDOM from 'react-dom'
+import React from 'react';
+import ReactDOM from 'react-dom';
+import Router from 'react-router/BrowserRouter';
+import Match from 'react-router/Match';
+import Miss from 'react-router/Miss';
+import Link from 'react-router/Link';
+import { syncHistoryWithStore } from 'react-router-redux';
+import createStore from './store/createStore';
+import AppContainer from './containers/AppContainer';
+import CoreLayout from './masterLayouts/CoreLayout/CoreLayout';
+import LoginPage from './components/layouts/LoginPage';
+import { NamedLink, matchRoutesToLocation, RoutesProvider, MatchWithRoutes } from 'react-router-addons-routes';
+import routes from './routes';
+import {Provider} from 'react-redux';
+import { OidcProvider } from 'redux-oidc';
+import userManager from './utils/userManager';
 
-
-
-import Router from 'react-router/BrowserRouter'
-import Match from 'react-router/Match'
-import Miss from 'react-router/Miss'
-import Link from 'react-router/Link'
-import MuiThemeProvider from './components/utils/materialStyles/MuiThemeProvider';
-
-
-
-import { syncHistoryWithStore } from 'react-router-redux'
-import createStore from './store/createStore'
-import AppContainer from './containers/AppContainer'
-import CoreLayout from './layouts/CoreLayout/CoreLayout'
-import LoginPage from './layouts/CoreLayout/LoginPage'
-
-import { NamedLink, matchRoutesToLocation, RoutesProvider, MatchWithRoutes } from 'react-router-addons-routes'
-
-
-const initialState = window.___INITIAL_STATE__
-const store = createStore(initialState)
+const initialState = window.___INITIAL_STATE__;
+const store = createStore(initialState);
 
 // ========================================================
 // Developer Tools Setup
 // ========================================================
 if (__DEBUG__) {
   if (window.devToolsExtension) {
-    window.devToolsExtension.open()
+    window.devToolsExtension.open();
   }
 }
 
-const routes = [
-  { pattern: '/home',
-    name: 'CoreLayout',
-    component: CoreLayout
-  },
-    { pattern: '/login',
-    name: 'LoginPage',
-    component: LoginPage
-  }
-]
 
 // ========================================================
 // Render Setup
@@ -50,17 +35,18 @@ const MOUNT_NODE = document.getElementById('root')
 
 let render = () => {
 
-
   ReactDOM.render(
-    <Router>
-    <MuiThemeProvider>
-      <RoutesProvider routes={routes}>
-        <div>
-          {routes.map(route => <MatchWithRoutes {...route}/>)}
-        </div>
-      </RoutesProvider>
-      </MuiThemeProvider>
-    </Router>,
+    <RoutesProvider routes={routes}>
+      <Provider store={store}>
+          <OidcProvider store={store} userManager={userManager}>
+              <Router>
+              <div>
+                {routes.map(route => <MatchWithRoutes {...route}/>)}
+              </div>
+              </Router>
+          </OidcProvider>
+      </Provider>
+    </RoutesProvider>,
     MOUNT_NODE
   )
 }
@@ -69,10 +55,9 @@ let render = () => {
 if (__DEV__) {
   if (module.hot) {
     // Development render functions
-    const renderApp = render
+    const renderApp = render;
     const renderError = (error) => {
       const RedBox = require('redbox-react').default
-
       ReactDOM.render(<RedBox error={error} />, MOUNT_NODE)
     }
 
@@ -88,8 +73,8 @@ if (__DEV__) {
     // Setup hot module replacement
     module.hot.accept('./routes/index', () => {
       setTimeout(() => {
-        ReactDOM.unmountComponentAtNode(MOUNT_NODE)
-        render()
+        ReactDOM.unmountComponentAtNode(MOUNT_NODE);
+        render();
       })
     })
   }
