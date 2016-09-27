e21d2a2fb9742561925d2c5e1a6ad159b97a657d
cehidi <dino.cehic@pearson.com>
2016-09-27 10:50:49 +1000

    Comment to test.

--- src/store/createStore.js
+++ src/store/createStore.js
@@ -58,7 +58,7 @@ export default (initialState = {}, history) => {
   }
 
   // sagas
-  sagaMiddleware.run(loadSubstriptionsSaga);
+  // sagaMiddleware.run(loadSubstriptionsSaga);
 
   return store;
 }
