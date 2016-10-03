85b280bbce0104b0bf74f2be2bcbd79ba21d7868
cehidi <dino.cehic@pearson.com>
2016-10-03 12:04:06 +1100

    Update.

---
+++ src/components/layouts/LoginPage/LoginPage.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import Button from '../../Button';
+import userManager from '../../utils/oidc/userManager';
+
+class LoginPage extends React.Component {
+
+  onLoginButtonClick = (event) => {
+    event.preventDefault();
+    userManager.signinRedirect();
+  };
+
+  onLogoutButtonClick = (event) => {
+    event.preventDefault();
+    userManager.signoutRedirect();
+  };
+
+
+  render() {
+
+    return (
+      <div>
+        <h1>Login Page</h1>
+        <Button label="Login using Identity Manager" onClick={this.onLoginButtonClick} />
+        <Button label="Log Out" onClick={this.onLogoutButtonClick} />
+      </div>
+    );
+  }
+
+}
+
+export default LoginPage;
---
+++ src/components/utils/oidc/userManager.js
@@ -0,0 +1,20 @@
+import { createUserManager } from 'redux-oidc';
+
+// user manager configuration object, see oidc-client-js documentation for details
+const config = {
+  client_id: 'VICRegWeb',
+  redirect_uri: 'http://localhost:8004/home',
+  response_type: 'id_token token',
+  scope: 'openid profile email roles VICRegScope all_claims',
+  authority: 'https://melbndevweb1.epenau.local:1301/ids',
+  post_logout_redirect_uri: 'http://localhost:8004/login',
+  silent_redirect_uri: 'http://localhost:8004/login',
+  automaticSilentRenew: true,
+  filterProtocolClaims: true,
+  loadUserInfo: true
+}
+
+// create an user manager instance
+const userManager = createUserManager(config);
+
+export default userManager;
