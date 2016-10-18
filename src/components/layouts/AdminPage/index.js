3ba99e978644122612ebc534a971d5f4dcaf30cf
Abdi Farah <abdi.farah@pearson.com>
2016-10-18 11:01:19 +1100

    Remove.

--- src/components/layouts/AdminPage/index.js
+++ src/components/layouts/AdminPage/index.js
@@ -71,7 +71,7 @@ class AdminPage extends React.Component {
                 </div>
                 <ul className="menu"></ul>
 
-                
+        
                 <div className="button-groups">
                   <Button label="Help" secondary={true} />
                   <Button label="Log Out" onClick={this.onLogoutButtonClicked} primary={true} />
