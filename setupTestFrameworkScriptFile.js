5a8b024f2c33b1ee64ae5a1939e22ce4c0570f46
abdi.farah@pearson.com <Abdi Farah>
2016-12-07 14:12:30 +1100

    fix test

--- setupTestFrameworkScriptFile.js
+++ setupTestFrameworkScriptFile.js
@@ -1,34 +1,28 @@
-import {jsdom} from 'jsdom';
+import { jsdom } from 'jsdom'
 
-  function storageMock() {
-    var storage = {};
+function storageMock () {
+  var storage = {}
 
-    return {
-      setItem: function(key, value) {
-        storage[key] = value || '';
-      },
-      getItem: function(key) {
-        return storage[key] || null;
-      },
-      removeItem: function(key) {
-        delete storage[key];
-      },
-      get length() {
-        return Object.keys(storage).length;
-      },
-      key: function(i) {
-        var keys = Object.keys(storage);
-        return keys[i] || null;
-      }
-    };
-  }
-
-
-const userManagerMock = {
-  _userStore: {
-    '_prefix': '33'
+  return {
+    setItem: function (key, value) {
+      storage[key] = value || ''
+    },
+    getItem: function (key) {
+      return storage[key] || null
+    },
+    removeItem: function (key) {
+      delete storage[key]
+    },
+    get length () {
+      return Object.keys(storage).length
+    },
+    key: function (i) {
+      var keys = Object.keys(storage)
+      return keys[i] || null
+    }
   }
 }
+
 const documentHTML = '<!doctype html><html><body><div id="root"></div></body></html>';
 global.document = jsdom(documentHTML)
 global.window = document.parentWindow
@@ -39,11 +33,4 @@ global.userManager = {
   _userStore: {
     '_prefix': jest.fn().mockReturnValueOnce(1)
   }
-}
-
-
-global.window.resizeTo = (width, height) => {
-  global.window.innerWidth = width || global.window.innerWidth
-  global.window.innerHeight = width || global.window.innerHeight
-  global.window.dispatchEvent(new Event('resize'))
 }
